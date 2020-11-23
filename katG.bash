3fab88150e913dc31114283c6ccb2c544f4516b7
Francisco Ascue <francisco.ascue131@gmail.com>
2020-11-23 00:02:31 -0500

    Tercer Producto

--- katG.bash
+++ katG.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 ####################################################################################################################
-#					FILTER RNA POLYMERASE SUBUNIT BETA					   #
+#						 FILTER KATG GENE					           #
 #						BY FRANCISCO ASCUE						   #
 #					email: francisco.ascue131@gmail.com					   #
 ####################################################################################################################
@@ -10,18 +10,18 @@
 GM="/home/fascue/INS/data/genomes"
 RES="/home/fascue/INS/results"
 
->>${RES}/multi_rpoB.fasta
+>>${RES}/multi_katG.fasta
 
 ##EXECUTION
 
 echo "Started at `date`"
 
 for i in $(cat $GM/codigos.txt);do
-	lis=( `fgrep -i "CDS" $GM/${i}.gff | fgrep -i "RNA polymerase subunit beta" | cut -f 9 | awk -F ';' '{print $2}'| sed -e 's/Parent=//g'` )
+	lis=( `fgrep -i "CDS" $GM/${i}.gff | fgrep -i "katg" | cut -f 9 | awk -F ';' '{print $2}'| sed -e 's/Parent=//g'` )
 	gffread -w $GM/transcrip.fa -g $GM/${i}.fna $GM/${i}.gff
 	samtools faidx $GM/transcrip.fa
-	samtools faidx $GM/transcrip.fa ${lis} >>${RES}/multi_rpoB.fasta
-	sed -i "s/${lis}/${i}/g" $RES/multi_rpoB.fasta
+	samtools faidx $GM/transcrip.fa ${lis} >>${RES}/multi_katG.fasta
+	sed -i "s/${lis}/${i}/g" $RES/multi_katG.fasta
 	rm $GM/*.fai $GM/transcrip.fa 
 done
 
