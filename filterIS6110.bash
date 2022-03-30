d4e2971fedaaef53d0408a7b2d133f09c8997b17
FranciscAscue <francisco.ascue131@gmail.com>
2022-03-30 18:04:15 -0500

    update

---
+++ filterIS6110.bash
@@ -0,0 +1,50 @@
+### filter IS6110
+
+sudo apt install bedtools
+
+## download fasta and gff file 
+
+efetch -db nucleotide -id NC_000962.3 -format fasta > NC_000962.3.fasta
+samtools faidx NC_000962.3.fasta
+
+## download from NCBI gff3 file
+
+### filter "mobile_genetic_element"
+fastaFromBed -fi NC_000962.3.fasta -bed <(awk '$3=="mobile_genetic_element"' NC_000962.3.gff3) -fo NC_000962.3_mobile.fasta
+
+## search IS6110 start and end positions 
+
+fgrep "IS6110" NC_000962.3.gff3 | fgrep -v "repeat_region" | fgrep -v "sequence_feature" | cut -f 4,5
+
+## search positions and match with names obtaein in NC_000962.3_mobile.fasta 
+
+fgrep "IS6110" NC_000962.3.gff3 | fgrep -v "repeat_region" | fgrep -v "sequence_feature" | awk '{print $4-1"-"$5}'
+
+## save in a list 
+
+list=( `fgrep "IS6110" NC_000962.3.gff3 | fgrep -v "repeat_region" | fgrep -v "sequence_feature" | awk '{print $4-1"-"$5}'` )
+
+### create a file to save filter sequences
+
+touch IS6110.fasta 
+
+## index NC_000962.3_mobile.fasta 
+
+samtools faidx NC_000962.3_mobile.fasta 
+
+## make a loop to filter 
+#### zsh loop
+for i in $list
+do
+	samtools faidx NC_000962.3_mobile.fasta NC_000962.3:${i} >> IS6110.fasta
+done
+
+#### bash loop 
+
+for i in ${list[@]};
+do
+	samtools faidx NC_000962.3_mobile.fasta NC_000962.3:${i} >> IS6110.fasta
+done
+
+
+### 
